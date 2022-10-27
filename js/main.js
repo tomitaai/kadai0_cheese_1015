f97a9abdd249a5a297384493bd552e4541c671dc
tomitaai <116848540+tomitaai@users.noreply.github.com>
2022-10-28 07:26:20 +0900

    初めてのファイルアップロード

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// jsを記述する際はここに記載していく
+
+
+{/* <script>　
+$(document).ready(function(){
+ 　$('.autoplay').slick({
+    slidesToShow: 3,
+    slidesToScroll:1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+          // 画像下のドット（ページ送り）を表示
+    dots:true,
+ 　});
+ });
+</script> */}
